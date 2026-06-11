592f867806386d7877b752348f402789be2cc91b
framelogy <framelogyprojects@gmail.com>
2026-06-11 22:10:17 +0700

    add welcome modal and edit survey page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Header from "../components/Header";
 import Hero from "../components/Hero";
 import SurveySection from "../components/SurveySection";
+import WelcomeModal from "../components/WelcomeModal";
 
 export default function HomePage() {
   return (
     <>
       <Header />
+      <WelcomeModal />
       <Hero />
       <SurveySection />
     </>
